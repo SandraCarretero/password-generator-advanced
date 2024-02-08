d1fb586287948f2de342fca1e16b413e66ca4301
SandraCarretero <sandracarretero24@gmail.com>
2024-02-08 09:33:28 +0100

    change length

--- src/js/password-functions.js
+++ src/js/password-functions.js
@@ -1,54 +1,60 @@
-import { PASSWORD_OPTIONS } from "./constants";
-import { buttonElement, passwordElement, passwordLengthElement, rangeElement } from "./dom";
-import { generateRandomNumber } from "./utils";
-
-
+import { PASSWORD_OPTIONS } from './constants';
+import {
+	buttonElement,
+	passwordElement,
+	passwordLengthElement,
+	rangeElement
+} from './dom';
+import { generateRandomNumber } from './utils';
 
 let alowedCharacters = '';
 let finalPassword = '';
-
-
+let passwordLength = rangeElement.value;
 
 const changeLengthText = () => {
-  passwordLengthElement.textContent = `${rangeElement.value}`;
+	passwordLengthElement.textContent = `${rangeElement.value}`;
 };
 
 const updatePasswordLength = event => {
-  PASSWORD_OPTIONS.passwordLength = event.target.value;
-  changeLengthText();
+	passwordLength = event.target.value;
+	changeLengthText();
 };
 
 const printFinalPassword = () => {
-  passwordElement.value = finalPassword;
+	passwordElement.value = finalPassword;
 };
 
 const generatePassword = () => {
-  finalPassword = '';
-  for (let index = 0; index < PASSWORD_OPTIONS.passwordLength; index++) {
-    const randomPosition = generateRandomNumber(alowedCharacters.length);
-    finalPassword += alowedCharacters.charAt(randomPosition);
-  }
-  printFinalPassword();
+	finalPassword = '';
+	for (let index = 0; index < passwordLength; index++) {
+		const randomPosition = generateRandomNumber(alowedCharacters.length);
+		finalPassword += alowedCharacters.charAt(randomPosition);
+	}
+	printFinalPassword();
 };
 
 const disabledButton = isDisabled => {
-  buttonElement.disabled = isDisabled;
+	buttonElement.disabled = isDisabled;
 };
 
 const getCheckedOptions = () => {
-  let isDisabled = true;
-  alowedCharacters = '';
+	let isDisabled = true;
+	alowedCharacters = '';
 
-  const allCheckboxes = document.querySelectorAll('.include-input:checked');
+	const allCheckboxes = document.querySelectorAll('.include-input:checked');
 
-  allCheckboxes.forEach(input => {
-    const optionId = input.id;
-    alowedCharacters += PASSWORD_OPTIONS[optionId];
-    isDisabled = false;
-  });
+	allCheckboxes.forEach(input => {
+		const optionId = input.id;
+		alowedCharacters += PASSWORD_OPTIONS[optionId];
+		isDisabled = false;
+	});
 
-  disabledButton(isDisabled);
+	disabledButton(isDisabled);
 };
 
-
-export {updatePasswordLength, generatePassword, getCheckedOptions, changeLengthText}
+export {
+	updatePasswordLength,
+	generatePassword,
+	getCheckedOptions,
+	changeLengthText
+};
